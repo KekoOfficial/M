c5647ab04d493ba1daf71b82a311f06dc5b2647c
NoaDev <143912708+KekoOfficial@users.noreply.github.com>
2026-06-13 12:32:43 -0300

    Crear config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+# Rutas de almacenamiento
+CARPETA_MUSICA = "/data/data/com.termux/files/home/storage/shared/Musica"
+CARPETA_TIKTOK = "/data/data/com.termux/files/home/storage/shared/TikTok"
+
+# Archivos de control
+ARCHIVO_ESTADO = "estado_descarga.txt"
+
+# Configuración de velocidad
+MAX_CONEXIONES = 10
+CALIDAD_MP3 = "320"
+
+# Contador global
+contador_duplicados = 0
